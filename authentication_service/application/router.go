fe8e88450725a5c9e0160b3719ff3313b51e2d14
bernardn38 <erisboxx@gmail.com>
2023-07-27 16:42:06 -0500

    added login route and some logging middleware

--- authentication_service/application/router.go
+++ authentication_service/application/router.go
@@ -1,15 +1,23 @@
 package application
 
 import (
+	"time"
+
 	"github.com/BernardN38/flutter-backend/handler"
-	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func SetupRouter(h *handler.Handler) *chi.Mux {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Timeout(60 * time.Second))
+
 	r.Get("/api/v1/auth/health", h.CheckHealth)
 	r.Post("/api/v1/auth/user", h.CreateUser)
+	r.Post("/api/v1/auth/user/login", h.LoginUser)
 	return r
 }
