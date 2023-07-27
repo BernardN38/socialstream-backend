c41aef043d57de7ddb9f7ddb83b8ff8b10f81366
bernardn38 <erisboxx@gmail.com>
2023-07-27 16:46:09 -0500

    updated removed redundant server starting

--- authentication_service/cmd/api/main.go
+++ authentication_service/cmd/api/main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
 	"github.com/BernardN38/flutter-backend/application"
 )
 
 func main() {
 	application.New().Run()
-	fmt.Println(http.ListenAndServe(":8080", nil))
 }
