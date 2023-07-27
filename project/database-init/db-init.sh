34397868554a3c52c79f27b2807ec90d422bab1f
bernardn38 <erisboxx@gmail.com>
2023-07-27 15:25:19 -0500

    remove creating tables in init config sincce services will be managing thier own migrations

--- project/database-init/db-init.sh
+++ project/database-init/db-init.sh
@@ -5,14 +5,4 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
   CREATE USER $APP_DB_USER WITH PASSWORD '$APP_DB_PASS';
   CREATE DATABASE $AUTH_DB_NAME;
   GRANT ALL PRIVILEGES ON DATABASE $AUTH_DB_NAME TO $APP_DB_USER;
-  \connect $AUTH_DB_NAME $APP_DB_USER
-  BEGIN;
-    CREATE TABLE users
-    (
-        id         serial PRIMARY KEY,
-        username   text NOT NULL UNIQUE,
-        email      text NOT NULL UNIQUE,
-        password   text NOT NULL
-    );
-  COMMIT;
 EOSQL
