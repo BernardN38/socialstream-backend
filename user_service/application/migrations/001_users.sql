85e9edff6fec3a0824b3867348e9285158396a90
bernardn38 <erisboxx@gmail.com>
2023-07-31 13:27:12 -0500

    update user schema

--- user_service/application/migrations/001_users.sql
+++ user_service/application/migrations/001_users.sql
@@ -3,10 +3,9 @@ CREATE TABLE users
 (
     id         serial PRIMARY KEY,
     username   text NOT NULL UNIQUE,
+    email      text NOT NULL UNIQUE,
     firstname text NOT NULL,
-    lastname text NOT NULL,
-    dob date NOT NULL
+    lastname text NOT NULL
 );
-
 -- +goose Down
 DROP TABLE users;
